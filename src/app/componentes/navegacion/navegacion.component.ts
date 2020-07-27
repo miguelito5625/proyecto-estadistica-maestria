6e7f7d78b81f61746ed3607b94a6d8896bb5d8c2
exbinario <miguel.4to.1994@gmail.com>
2020-07-27 13:10:09 -0600

    se agrego un boton en la barra de navegacion para poder visualizar los integrantes

--- src/app/componentes/navegacion/navegacion.component.ts
+++ src/app/componentes/navegacion/navegacion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var Swal:any;
+
 @Component({
   selector: 'app-navegacion',
   templateUrl: './navegacion.component.html',
@@ -10,6 +12,16 @@ export class NavegacionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  mostrarIntegrantes(){
+    Swal.fire({
+      title: 'Integrantes',
+      html: `1. Marco Antonio Villagran Sagastume <br>
+              2. Oscar Giovany Reyes Muñoz <br>
+              3. Miguel Angel Archila García`
+    });
   }
 
 }
